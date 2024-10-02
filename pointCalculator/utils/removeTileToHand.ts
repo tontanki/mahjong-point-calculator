780b648febd16912fc3d988b540bbc401f5fee58
tontanki <dragonnight612@gmail.com>
2024-10-02 15:20:48 +0900

    Add files via upload

---
+++ pointCalculator/utils/removeTileToHand.ts
@@ -0,0 +1,17 @@
+import { Tile, TilesList } from '@types';
+
+export const removeTileToHaishi = (haishi: TilesList, addTile: Tile): void => {
+    const tileType = addTile.name[0] as 'm' | 'p' | 's' | 'z';
+    const tileNumber = Number(addTile.name[1]) as
+        | 1
+        | 2
+        | 3
+        | 4
+        | 5
+        | 6
+        | 7
+        | 8
+        | 9;
+
+    haishi[tileType][tileNumber]--;
+};
